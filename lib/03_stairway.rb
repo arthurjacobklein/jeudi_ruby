23db258699fa104942dd6be495f601d1b75cd78c
forkhembo <forkhembo@riseup.net>
2019-04-04 17:24:19 +0200

    Dice creation and how many dice you want to roll

---
+++ lib/03_stairway.rb
@@ -0,0 +1,20 @@
+=begin 
+Tu vas maintenant coder un super jeu qui déchire. Mario n'a qu'à bien se tenir avec sa pyramide ! Nous allons imaginer un programme dans lequel une gentille personne va devoir monter 10 marches en fonction d'un jet de dé. Une version informatique du jeu de l'oie en quelque sorte !
+
+Voici comment cela va se dérouler : à l'exécution, le programme lance une partie. Le joueur est tout en bas d'un escalier à 10 marches, et à chaque tour il va lancer un dé :
+
+S’il fait 5 ou 6, il avance d'une marche et le programme le lui dit (ainsi que la marche où il est à présent)
+S’il fait 1, il descend d'une marche et le programme le lui dit (ainsi que la marche où il est à présent)
+S’il fait 2, 3, ou 4 rien ne se passe, et le programme le lui dit (ainsi que la marche où il est resté)
+Quand le joueur atteint la 10ème marche, le programme l'en informe avec un message super enthousiaste, et le jeu se termine.
+=end
+
+puts "How many dice do you want to roll?"
+    roll = gets.to_i
+    num_rolls = roll
+    y = 0
+    x = y.to_i + 1
+    while x <= roll
+    puts "Roll " + x.to_s + ": " + rand(1..6).to_s
+    x += 1
+    end
