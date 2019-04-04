8be923d204f77b8f8eed900b8fdab2ec19fc04dc
forkhembo <forkhembo@riseup.net>
2019-04-04 19:03:50 +0200

    remove array and just use score to see the result

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,14 +1,4 @@
 =begin 
-Tu vas maintenant coder un super jeu qui déchire. Mario n'a qu'à bien se tenir avec sa pyramide ! Nous allons imaginer un programme dans lequel une gentille personne va devoir monter 10 marches en fonction d'un jet de dé. Une version informatique du jeu de l'oie en quelque sorte !
-
-Voici comment cela va se dérouler : à l'exécution, le programme lance une partie. Le joueur est tout en bas d'un escalier à 10 marches, et à chaque tour il va lancer un dé :
-
-S’il fait 5 ou 6, il avance d'une marche et le programme le lui dit (ainsi que la marche où il est à présent)
-S’il fait 1, il descend d'une marche et le programme le lui dit (ainsi que la marche où il est à présent)
-S’il fait 2, 3, ou 4 rien ne se passe, et le programme le lui dit (ainsi que la marche où il est resté)
-Quand le joueur atteint la 10ème marche, le programme l'en informe avec un message super enthousiaste, et le jeu se termine.
-=end
-
 def number_dice
 puts "Bienvenue dans le super jeu qui déchire\n Combien de dés voulez-vous lancer ?"
     roll = gets.to_i
@@ -19,31 +9,42 @@ puts "Bienvenue dans le super jeu qui déchire\n Combien de dés voulez-vous lan
     puts "Roll " + x.to_s + ": " + rand(1..6).to_s
     x += 1
     end
-end
+end 
+=end
 
 def lancement_de
-i = 0
-score = Array.new
 puts "Bienvenue dans le super jeu qui déchire\n Lancement du dé"
-while i < 10
-n = rand (1..6)
+rand (1..6)
+end
+
+
+score = 0
+
+while score < 10
+dice_result = lancement_de
+
+
 puts "Le dé a fait : #{n}"
-    if n == 5 || n == 6
-        m = 1
-        puts "Vous avancez d'une marche, vous êtes sur la marche #{m}"
-        score << m
-    elsif n == 2 || n == 3 || n == 4
-        m = 0
-        puts "Rien ne se passe vous êtes sur la marche #{m}"
-        score << m
-    else n == 1
-        m = -1
-        puts "Vous reculez d'une marche, vous êtes sur la marche #{m}"
-        score << m
+    if dice_result == 5 || dice_result == 6
+        score += 1
+        puts "Vous avancez d'une marche, vous êtes sur la marche #{score}"
+
+    elsif dice_result == 2 || dice_result == 3 || dice_result == 4
+        score += 0
+        puts "Rien ne se passe vous êtes sur la marche #{score}"
+
+    else dice_result == 1
+        score -= 1
+        puts "Vous reculez d'une marche, vous êtes sur la marche #{score}"
     end
    
-    i = score.sum
-    puts i
+=begin     i = score.sum
+=end    
+ puts "Votre score est #{i}"
+
+    if i < 10
+        puts "Relance"
+
 
 end
 end
