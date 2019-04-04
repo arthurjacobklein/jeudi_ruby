b404170230918324871bc5cbc470564507f7b898
forkhembo <forkhembo@riseup.net>
2019-04-05 00:35:48 +0200

    bug fixed: if you put 2 wrong passwords before, you could enter the secret_zone

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,32 +1,30 @@
 def signup
-  puts "Crée ton mdp?"
+  puts "Choisis un mot de passe."
   print "> "
   password = gets.chomp
-  
-
-  puts "Quel est ton mot de passe ?"
-  print "> "
-  login = gets.chomp
-  
-
- if login == password 
-    puts "Bienvenue, nous avons la réponse à création de l'univers, appuie sur entrer pour connaître la réponse"
-    gets.chomp
-    puts "42" end
-
-
-  while login != password 
-    puts "Mauvais mot de passe, réessayer"
-    puts "Quel est ton mot de passe ?"
-    print "> "
-    try = gets.chomp
-    login == password 
-    puts "Bienvenue, nous avons la réponse à la création de l'univers, appuie sur entrer pour connaître la réponse"
-    gets.chomp
-    puts "42"
-    break
-  end
-  
 end
 
-signup
+$password = signup
+
+def secret_zone
+  puts "Bienvenue, nous avons la réponse à la création de l'univers, appuie sur entrer pour connaître la réponse"
+  gets.chomp
+  puts "42"
+end
+
+def log_in
+  $password
+  puts "Quel est ton mot de passe ?"
+  type = gets.chomp
+      while type != $password
+      puts "Mauvais mot de passe, réessayer"
+      type = gets.chomp
+  end
+end
+
+def perform
+  log_in
+  secret_zone
+end
+
+perform
